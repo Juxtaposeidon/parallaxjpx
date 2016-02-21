3f4d7e71de937a167a8772b6f87069ba3b9caced
dbcjckyee <dbcjckyee@gmail.com>
2016-02-20 22:54:08 -0800

    start fixing projects

--- juxtaposeidon/app/assets/javascripts/application.js
+++ juxtaposeidon/app/assets/javascripts/application.js
@@ -14,3 +14,36 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+addOverlay = function(image){
+  $("#overlay").width(image.css("width"));
+  $("#overlay").height(image.css("height"));
+  $("#overlay").css("top", image.offset().top + "px");
+  $("#overlay").css("left", image.offset().left + "px");
+  $("#overlay").html('<div class = "imagelabel">' + image.attr('id') + '</div>');
+}
+
+removeOverlay = function(){
+  $("#overlay").width(0);
+  $("#overlay").height(0);
+  $("#overlay").html('');
+}
+
+$(document).ready(function(){
+  $('.project').mouseenter(function(){
+    addOverlay($(this));
+  });
+
+  $('#overlay').mouseleave(function(){
+    removeOverlay();
+  });
+
+  $('#overlay').click(function(){
+    $.ajax({
+      data: {key: $(this).text()},
+      url: '/website/projects/show'
+    })
+    .done(function(result){
+      $('#projecttext').html(result['partial'])
+    })
+  })
+})
